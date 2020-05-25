6a61fbccaf304de2fc0f6e65dc5c6b45cd308b01
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-05-25 13:40:43 +0000

    updated files

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // JavaScript File
 
-// variables assigned to calculate the area and circumference of a circle
-function Area(){
-var diameter = document.getElementById("diameter");
-var radius = document.getElementById("radius");
-var diametervalue = +diameter.value;
-var radiusvalue = +(diameter.value/2);
-var result1 = ((diameter.value/2)*(diameter.value/2)*Math.PI);
-var result2  = (diameter.value*Math.PI);
-var divresult1 = document.getElementById("divresult1");
-var divresult2 = document.getElementById("divresult2");
-divresult1.innerHTML = result1;
-divresult2.innerHTML = result2;
+document.getElementById("submitguess").onclick = function() {
+var number = document.getElementById("number");
+if (number < 0) {
+  alert("Your number is negative!");
 }
+else if (number > 0) {
+  alert("Your number is positive");
+}
+else {
+  alert("Please type in a number first!");
+}
+};
