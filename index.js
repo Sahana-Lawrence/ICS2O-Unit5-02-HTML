1585847b62b4e4ee9bfe78a621c972a776fd3abe
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-05-29 00:32:43 +0000

    updated files

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 document.getElementById("submitguess").onclick = function() {
 var input = document.getElementById("input");
-if (input < 0) {
-  alert("Your number is negative!");
+var inputvalue = input.value;
+
+ if (input.value < 0) {
+   alert("Your number is negative!");
 }
-else if (input >= 0) {
-  alert("Your number is positive!");
+ else if (input.value > 0) {
+   alert("Your number is positive!");
 }
-else {
+ else {
   alert("Please type in a number first!");
 }
 };
